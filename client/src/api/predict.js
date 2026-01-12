c022cfcb2d65b9dd0caa93adf0d652d97628013b
ianwright27 <thewian27@gmail.com>
2026-01-12 19:19:36 +0300

    public api

--- client/src/api/predict.js
+++ client/src/api/predict.js
@@ -1,7 +1,7 @@
 export async function predictSmoke(data) {
   const LOCAL_API_URL = "http:///localhost:8000/predict";
   const PRODUCTION_API_URL = import.meta.env.VITE_API_URL; 
-  const response = await fetch(LOCAL_API_URL, {
+  const response = await fetch(PRODUCTION_API_URL, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
